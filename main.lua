024ef58f8395d2f36ea7cbc74bdde67385c7ae17
12321h3uu91hrfoegrfq <jelloplatinum@gmail.com>
2026-06-09 20:26:28 -0500

    Replace all GitHub links with own repo and change vape/ to Real/

--- main.lua
+++ main.lua
@@ -37,7 +37,7 @@ local function downloadFile(path, func)
 			error(res)
 		end
 		if path:find('.lua') then
-			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after Real updates.\n'..res
 		end
 		writefile(path, res)
 	end
